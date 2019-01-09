2c850b82db96f434a81345c663b61e81beb0b86d
Djordje <djoledell@gmail.com>
2019-01-09 11:34:04 +0100

    Izbor sobe
    Dodate kontrole u ChooseRoomActivity za prvi sprat.
    Uradjena logika prosledjivanja.
    Uradjen refactoring

--- app/src/main/java/com/example/windows10/androidmuzej/ChooseRoomActivity.java
+++ app/src/main/java/com/example/windows10/androidmuzej/ChooseRoomActivity.java
@@ -1,27 +1,53 @@
 package com.example.windows10.androidmuzej;
 
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageButton;
 
-import java.util.Locale;
-
-public class ChooseRoomActivity extends AppCompatActivity {
+public class ChooseRoomActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_room);
 
-        //Print to log current localization
-        Configuration configuration = getResources().getConfiguration();
-        Log.i("Localization", configuration.locale.getDisplayLanguage());
+        //Declaring buttons
+        ImageButton btnRoom1Up = findViewById(R.id.btnRoom1Up);
+        ImageButton btnRoom1Down = findViewById(R.id.btnRoom1Down);
+        ImageButton btnRoom2 = findViewById(R.id.btnRoom2);
+        ImageButton btnRoom3 = findViewById(R.id.btnRoom3);
+        ImageButton btnRoom4Up = findViewById(R.id.btnRoom4Up);
+        ImageButton btnRoom4Down = findViewById(R.id.btnRoom4Down);
+        ImageButton btnUp = findViewById(R.id.btnUp);
 
+        //Setting listener
+        btnRoom1Up.setOnClickListener(this);
+        btnRoom1Down.setOnClickListener(this);
+        btnRoom2.setOnClickListener(this);
+        btnRoom3.setOnClickListener(this);
+        btnRoom4Up.setOnClickListener(this);
+        btnRoom4Down.setOnClickListener(this);
+        btnUp.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View v) {
+        String roomNumber = v.getContentDescription().toString();
+
+        if(roomNumber.equals("5"))
+        {
+            Log.i("Room number", "Second floor");
+        }
+        else
+        {
+            Log.i("Room number", "Drugi sprat");
+//            Intent intent = new Intent(ChooseRoomActivity.this, RoomActivity.class);
+//            intent.putExtra("roomNumber", roomNumber);
+//            startActivity(intent);
+        }
+    }
 }
