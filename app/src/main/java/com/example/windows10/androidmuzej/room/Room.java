8b4116e84a8b67b60c07178a4d634a50cd89e30d
Djordje <djoledell@gmail.com>
2019-09-14 14:09:06 +0200

    Refactoring

---
+++ app/src/main/java/com/example/windows10/androidmuzej/room/Room.java
@@ -0,0 +1,59 @@
+package com.example.windows10.androidmuzej.room;
+
+import android.graphics.drawable.Drawable;
+
+import com.example.windows10.androidmuzej.audioPlayer.AudioPlayerItem;
+
+import java.util.ArrayList;
+
+public class Room {
+
+    private int roomNumber;
+    private String title = "";
+    private Drawable logo;
+    private ArrayList<Page> pages = new ArrayList<>();
+    private ArrayList<AudioPlayerItem> audioItems = new ArrayList<>();
+    private String videoPath = "";
+
+
+    public Room(int roomNumber)
+    {
+        this.roomNumber = roomNumber;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Drawable getLogo() {
+        return logo;
+    }
+
+    public void setLogo(Drawable logo) {
+        this.logo = logo;
+    }
+
+    public ArrayList<Page> getPages() {
+        return pages;
+    }
+
+    public void setPages(ArrayList<Page> pages) {
+        this.pages = pages;
+    }
+
+    public ArrayList<AudioPlayerItem> getAudioItems() {
+        return audioItems;
+    }
+
+    public String getVideoPath() { return videoPath; }
+
+    public void setVideoPath(String videoPath) { this.videoPath = videoPath; }
+}
